b0ffabb86bb7cab24a6cefe7767d38c92d889b5f
kanibek <c.design.develope@gmail.com>
2012-08-30 11:25:29 +0400

    some changes

--- FacebookChat/MasterViewController.m
+++ FacebookChat/MasterViewController.m
@@ -32,12 +32,9 @@
 							
 - (void)dealloc
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self 
-                                                    name:@"facebookAuthorized" 
-                                                  object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self 
-                                                    name:@"messageCome" 
-                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+  
     [_detailViewController release];
     [__fetchedResultsController release];
     [__managedObjectContext release];
