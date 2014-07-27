b7ec85edf52346b1f08e0172b0d3036a4c715d17
kanibek <c.design.develope@gmail.com>
2014-07-27 19:12:15 +0600

    Added base view controllers.

---
+++ FCUI/FCMainVC/FCBaseTVC.h
@@ -0,0 +1,13 @@
+//
+//  FCBaseTVC.h
+//  FacebookChat
+//
+//  Created by Kanybek Momukeev on 27/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCBaseTVC : UITableViewController
+
+@end
---
+++ FCUI/FCMainVC/FCBaseVC.h
@@ -0,0 +1,13 @@
+//
+//  FCBaseVC.h
+//  FacebookChat
+//
+//  Created by Kanybek Momukeev on 27/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCBaseVC : UIViewController
+
+@end
